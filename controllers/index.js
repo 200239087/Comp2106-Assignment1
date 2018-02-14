cf2491707caea3052a67db95ab8ffa54bd89ab17
200239087 <200239087@student.georgianc.on.ca>
2018-02-14 17:17:12 -0500

    added pages and connected links

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,29 @@ let express = require('express');
 let router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', (req, res, next) => res.render('index', { 
+  title: 'COMP2106 - Assignment 1',
+  content: '' 
+}));
+
+router.get('/about', (req, res, next) => res.render('about', {
+  title: 'About',
+  content: ''
+}));
+
+router.get('/projects', (req, res, next) => res.render('projects', {
+  title: 'Projects',
+  content: ''
+}));
+
+router.get('/services', (req, res, next) => res.render('services', {
+  title: 'Services',
+  content: ''
+}));
+
+router.get('/contact', (req, res, next) => res.render('contact', {
+  title: 'Contact',
+  content: ''
+}));
 
 module.exports = router;
