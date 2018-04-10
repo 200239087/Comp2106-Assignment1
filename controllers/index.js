2de78f6be377ed9ff49d98134c96c21d0fa33639
200239087 <200239087@student.georgianc.on.ca>
2018-04-10 12:02:36 -0400

    Professional tweaks

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => res.render('index', { 
-  title: 'COMP2106 - Assignment 1',
+  title: 'Ryan Quigley | Portfolio',
   content: 'Home Page' 
 }));
 
@@ -15,11 +15,6 @@ router.get('/about', (req, res, next) => res.render('about', {
 router.get('/projects', (req, res, next) => res.render('projects', {
   title: 'Projects',
   content: 'Projects Page'
-}));
-
-router.get('/services', (req, res, next) => res.render('services', {
-  title: 'Services',
-  content: 'Services Page'
 }));
 
 router.get('/contact', (req, res, next) => res.render('contact', {
